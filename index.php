03e87d24b0f9382dbd0260f22a2f61f2469ba61c
Isabella Qiu <55269480+Isabella-Qiu@users.noreply.github.com>
2021-09-09 01:07:18 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links <br> Isabella Qiu Period 2</p>'; ?> 
+    <ul>
+      <li><a href="home.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Projects.html">Projects.html</a></li></br>
+      <li><a href="WorkExperience.html">WorkExperience.html</a></li></br>
+      <li><a href="ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html
